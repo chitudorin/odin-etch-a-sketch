a0fbab28df5267064f78869e9afaac0c6b5922a1
chitudorin <dorinchitu112@gmail.com>
2022-12-29 21:14:22 +0200

    Add prompt to change play area resolution

--- script.js
+++ script.js
@@ -1,38 +1,55 @@
-container = document.querySelector("div.container");
-
-for (i = 0; i <= 14; i++) {
-  row = createRow();
-  for (j = 0; j <= 14; j++) {
-    row.appendChild(createNewElement());
-  }
-  container.appendChild(row);
-}
-
 function createNewElement() {
-  let element = document.createElement("div");
-  element.classList.add("element");
-  return element;
+    let element = document.createElement("div");
+    element.classList.add("element");
+    return element;
 }
 
 function createRow() {
-  let row = document.createElement("div");
-  row.classList.add("row");
-  return row;
+    let row = document.createElement("div");
+    row.classList.add("row");
+    return row;
 }
 
 function resetPlayArea() {
-  let elements = document.querySelectorAll("div.element");
-  elements.forEach((element) => {
-    element.style.backgroundColor = "red";
-  });
+    let elements = document.querySelectorAll("div.element");
+    elements.forEach((element) => {
+        element.style.backgroundColor = "red";
+    });
 }
 
-document.querySelector("button").addEventListener("click", resetPlayArea);
+function deletePlayArea() {
+    document.querySelectorAll("div.element").forEach(e => e.remove());
+    document.querySelectorAll("div.row").forEach(e => e.remove());
+}
 
-elements = document.querySelectorAll("div.element");
+function promptPlayArea() {
+    let n = prompt("How many pixels? (max 100)")
+    deletePlayArea();
+    generatePlayArea(n)
+}
 
-elements.forEach((element) => {
-  element.addEventListener("mouseover", () => {
-    element.style.backgroundColor = "black";
-  });
-});
+function generatePlayArea(n) {
+    if (n > 100) n = 100;
+    for (i = 0; i <= n; i++) {
+        let row = createRow();
+        for (j = 0; j <= n; j++) {
+            row.appendChild(createNewElement());
+        }
+        container.appendChild(row);
+    }
+
+    elements = document.querySelectorAll("div.element");
+    elements.forEach((element) => {
+        element.addEventListener("mouseover", () => {
+            element.style.backgroundColor = "black";
+        });
+    });
+}
+
+
+container = document.querySelector("div.container");
+document.querySelector("button.reset").addEventListener("click", resetPlayArea);
+document.querySelector("button.change").addEventListener("click", promptPlayArea);
+generatePlayArea(10);
+
+
